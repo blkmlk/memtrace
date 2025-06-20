76b112ede15cc0d86e58539bc8982c9a775c98d2
Islam Bekbuzarov <n157318@gmail.com>
2025-06-20 23:41:42 +0300

    modified README.md

--- README.md
+++ README.md
@@ -1,9 +1,16 @@
 # memtrace
 
+
 ## MemTrace
 
+
 ![example](example.svg)
+
 A Rust-based tool for visualizing heap memory consumption using flamegraphs. It helps you profile your app
+
+> ⚠️ **Warning:** at the moment the tool requires to download a dynamic library to work. The library is opensource, you can find it [here](https://github.com/blkmlk/memtrace-lib)
+>
+> If you know a better solution - welcome to pull requests
 
 ### Installation
 
@@ -12,7 +19,6 @@ cargo install memtrace
 ```
 
 ### Usage
-
 ```bash
 memtrace <your_program>
 ```
--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,13 @@
 //! # MemTrace
 //!
 //!
-//! **HeapFlame** is a Rust-based tool for visualizing heap memory usage over time using flamegraphs. It helps you identify allocation-heavy code paths and memory bottlenecks in your Rust applications.
 //! ![example](example.svg)
+//!
+//! A Rust-based tool for visualizing heap memory consumption using flamegraphs. It helps you profile your app
+//!
+//! > ⚠️ **Warning:** at the moment the tool requires to download a dynamic library to work. The library is opensource, you can find it [here](https://github.com/blkmlk/memtrace-lib)
+//! >
+//! > If you know a better solution - welcome to pull requests
 //!
 //! ## Installation
 //!
